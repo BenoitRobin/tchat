c87dde5b33070da4dcd6663cebbe3d14dbd3ee3c
Benoit ROBIN <benoitrobin@me.com>
2020-09-04 16:11:28 +0200

    add HTMLcontent in FormMessage

--- src/components/FormMessage/index.js
+++ src/components/FormMessage/index.js
@@ -1,11 +1,39 @@
 // == Import : npm
 import React from 'react';
+import PropTypes from 'prop-types';
 
 // == Import : local
 import './styles.scss';
 
 // == Composant
-const FormMessage = () => <div>Formulaire du message</div>;
+const FormMessage = ({ messageText }) => (
+  <form
+    className="message-form"
+    onSubmit={(event) => {
+      event.preventDefault();
+      console.log('clic');
+    }}
+  >
+    <input
+      type="text"
+      className="form-input"
+      value={messageText}
+      onChange={(event) => {
+        const text= event.target.value;
+        console.log(text);
+      }}
+    />
+    <button
+      className="form-button"
+      type="submit"
+    >
+      &gt;
+    </button>
+  </form>
+);
 
+FormMessage.propTypes = {
+  messageText: PropTypes.string.isRequired,
+};
 // == Export
 export default FormMessage;
