104bf03f70b62d5228e66a6c38af5e01f970a849
Benoit ROBIN <benoitrobin@me.com>
2020-09-04 18:01:08 +0200

    Add fake datas & props to components

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -6,11 +6,40 @@ import './styles.scss';
 import ListMessages from '../ListMessages';
 import FormMessage from '../FormMessage';
 
+const data = [
+  {
+    id: 1,
+    author: 'Benoit Robin',
+    content: 'Salut comment ça va ?',
+  },
+  {
+    id: 2,
+    author: 'Elon Musk',
+    content: 'Va bien, on fait couler!',
+  },
+  {
+    id: 3,
+    author: 'Steve Jobs',
+    content: 'Qu\'est-ce tu racontes?',
+  },
+  {
+    id: 4,
+    author: 'Steve Wozniak',
+    content: 'H\é ! les gars je suis là!!',
+  },
+  {
+    id: 5,
+    author: 'Benoit Robin',
+    content: '....',
+  },
+  
+];
+
 // == Composant
 const App = () => (
   <div className="app">
-    <ListMessages />
-    <FormMessage />
+    <ListMessages list={data} />
+    <FormMessage messageText="" />
   </div>
 );
 
--- src/components/ListMessages/index.js
+++ src/components/ListMessages/index.js
@@ -1,20 +1,41 @@
 // == Import : npm
 import React from 'react';
+import PropTypes from 'prop-types';
 
 // == Import : local
 import './styles.scss';
 
 // == Composant
-const ListMessages = () => (
-  <Message />
-);
+const ListMessages = ({ list }) => (
+  <div className="message-container">
+    {
+    list.map((listObject) => (
 
-const Message = () => (
+      <Message key={listObject.id} {...listObject} />
+    ))
+    }
+  </div>
+
+);
+ListMessages.propTypes = {
+  list: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+    }),
+  ).isRequired,
+};
+
+const Message = ({ author, content }) => (
   <div className="message">
-    <div className="message-author">Benoit Robin</div>
-    <p className="message-content">Comment ça va ?</p>
+    <div className="message-author">{ author }</div>
+    <p className="message-content">{ content }</p>
   </div>
 );
+
+Message.propTypes = {
+  author: PropTypes.string.isRequired,
+  content: PropTypes.string.isRequired,
+};
 
 // == Export
 export default ListMessages;
